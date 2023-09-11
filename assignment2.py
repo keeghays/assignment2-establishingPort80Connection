795593fb58b89d006dd8da7dfe2eab39106a161d
keeghays <114433622+keeghays@users.noreply.github.com>
2023-09-10 22:26:47 -0400

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -5,59 +5,60 @@ class Assignment2:
         self.year = year
 
     def tellAge(self, currentYear):
-        birth_year = currentYear - self.year
-        print(f'Your age is {birth_year}')
+        age = currentYear - self.year
+        print(f"Your age is {age}")
 
     def listAnniversaries(self):
-        current_year = 2022
+        today = 2022
         anniversaries = []
-        for i in range(1, 11):
-            anniversary = current_year - self.year + (i * 10)
-            anniversaries.append(anniversary)
+        for i in range(10, 101, 10):
+            if self.year + i <= today:
+                anniversaries.append(i)
         return anniversaries
 
     def modifyYear(self, n):
         year_str = str(self.year)
-        result = year_str[:2] * n + year_str[-2:] * n
+        result = year_str[:2] * n
+        odd_chars = year_str[::2]
+        result += odd_chars * n
         return result
 
     @staticmethod
     def checkGoodString(string):
         if len(string) < 9:
             return False
-        if not string[0].islower():
+        if not string[0].islower() or not string[0].isalpha():
             return False
-        digits = 0
-        for char in string:
-            if char.isdigit():
-                digits += 1
-        if digits != 1:
+        digit_count = sum(1 for char in string if char.isdigit())
+        if digit_count != 1:
             return False
         return True
 
     @staticmethod
     def connectTcp(host, port):
         try:
-            socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-            return True
-        except Exception as e:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(5)  # Set a timeout for the connection attempt
+                s.connect((host, port))
+                return True
+        except (socket.timeout, ConnectionRefusedError):
             return False
 
 # Example usage:
 a = Assignment2(2000)
 a.tellAge(2023)
-
 ret = a.listAnniversaries()
 print(ret)
 
-modified_year = a.modifyYear(5)
-print(modified_year)
+a = Assignment2(1782)
+ret = a.modifyYear(3)
+print(ret)
 
-print(Assignment2.checkGoodString("f1obar0more"))
-print(Assignment2.checkGoodString("foobar0more"))
+ret = Assignment2.checkGoodString("f1obar0more")
+print(ret)
 
-retval = Assignment2.connectTcp("www.google.com", 80)
-if retval:
+ret = Assignment2.connectTcp("www.google.com", 80)
+if ret:
     print("Connection established correctly")
 else:
     print("Some error")
